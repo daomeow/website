44572fc91c64511b7fb81f3a23fd09a90291c591
Melanie Daoheuang <daoh0031@gmail.com>
2022-08-29 15:20:49 -0600

    Create and style navigation buttons

---
+++ src/components/App/App.tsx
@@ -0,0 +1,10 @@
+import Header from '../Header/Header';
+import './App.css';
+
+
+const App = () => {
+  return (
+    <Header/>
+  )
+}
+export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <section className="navigation-container">
+      <button className="navigation-buttons">About</button>
+      <button className="navigation-buttons">Projects</button>
+      <button className="navigation-buttons">Photos</button>
+    </section>
+    
+  )
+}
+
+export default Header;
