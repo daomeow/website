c265924b106d7a6d9a818b6d2c422ea2ba602f47
Melanie Daoheuang <daoh0031@gmail.com>
2022-08-30 13:58:48 -0600

    Add conditional rendering for home nav button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,15 @@
 import "./Header.css";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 const Header = () => {
+  const location = useLocation();
+
   return (
     <section className="navigation-container">
-      <NavLink to="/" className="navigation-buttons">Home</NavLink>
+      {location.pathname !== "/" &&
+        <NavLink to="/" className="navigation-buttons">Home</NavLink>
+      }
+      <NavLink to="/about" className="navigation-buttons">About</NavLink>
       <NavLink to="/projects" className="navigation-buttons">Projects</NavLink>
       <NavLink to="/photos" className="navigation-buttons">Photos</NavLink>
     </section>
