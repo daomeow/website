966dfed119e026dc365523c1c4da64341b5bceaa
Melanie Daoheuang <daoh0031@gmail.com>
2022-08-30 15:37:14 -0600

    Refactor nav buttons and add GIF

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,15 +3,26 @@ import { NavLink, useLocation } from "react-router-dom";
 
 const Header = () => {
   const location = useLocation();
+  const buttons = [
+    {id: "/", name: "Home"}, 
+    {id: "/about", name: "About"}, 
+    {id: "/projects", name: "Projects"}, 
+    {id: "/photos", name: "Photos"}
+  ];
+
+  const createButtons = () => {
+    return (
+      buttons.map(nav => {
+        if (nav.id !== location.pathname) {
+          return <NavLink to={nav.id} className="navigation-buttons">{nav.name}</NavLink>
+        }
+      })
+    )
+  }
 
   return (
     <section className="navigation-container">
-      {location.pathname !== "/" &&
-        <NavLink to="/" className="navigation-buttons">Home</NavLink>
-      }
-      <NavLink to="/about" className="navigation-buttons">About</NavLink>
-      <NavLink to="/projects" className="navigation-buttons">Projects</NavLink>
-      <NavLink to="/photos" className="navigation-buttons">Photos</NavLink>
+      {createButtons()}
     </section>
   )
 }
